cb33acb0f4e07db563e2f331e6c850998f34337e
intrudershanky <intrudershanky@outlook.com>
2017-04-05 21:04:19 +0530

    Functions

---
+++ functions/index.js
@@ -0,0 +1,53 @@
+var functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+	response.send(request);
+})
+
+exports.sendFollowerNotification = functions.database.ref('/data').onWrite(event => {
+	 const snapshot = event.data;
+  // Only send a notification when a message has been created.
+  if (snapshot.previous.val()) {
+    return;
+  }
+
+  // Notification details.
+  const text = snapshot.val().message;
+  const payload = {
+    notification: {
+      title: `${snapshot.val().name} posted ${text ? 'a message' : 'an image'}`,
+      body: text ? (text.length <= 100 ? text : text.substring(0, 97) + '...') : '',
+      icon: snapshot.val().photoUrl || '/images/profile_placeholder.png',
+      click_action: `https://${functions.config().firebase.authDomain}`
+    }
+  };
+
+  // Get the list of device tokens.
+  return admin.database().ref('token').once('value').then(allTokens => {
+    if (allTokens.val()) {
+      // Listing all tokens.
+      console.log(allTokens.val());
+      const tokens = Object.keys(allTokens.val());
+
+      // Send notifications to all tokens.
+      return admin.messaging().sendToDevice(tokens, payload).then(response => {
+        // For each message check if there was an error.
+        const tokensToRemove = [];
+        response.results.forEach((result, index) => {
+          const error = result.error;
+          if (error) {
+            console.error('Failure sending notification to', tokens[index], error);
+            // Cleanup the tokens who are not registered anymore.
+            if (error.code === 'messaging/invalid-registration-token' ||
+                error.code === 'messaging/registration-token-not-registered') {
+              tokensToRemove.push(allTokens.ref.child(tokens[index]).remove());
+            }
+          }
+        });
+        return Promise.all(tokensToRemove);
+      });
+    }
+  });
+});
